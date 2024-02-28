27c1e8781c237f85e50b3c822f9bc0e4e8b3e7e1
EloyG3186 <eloy.gonzalezja2@gmail.com>
2024-02-28 03:03:56 -0500

    Actualizo tabla de clima

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -62,22 +62,24 @@ let loadWeekForecastData = () => {
 
     let week = document.getElementsByClassName("list-group")
     let i = 0
+    let lista = ""
     console.log(week)
     for (let dia of guayaquil.forecast_week) {
-        week[i].innerHTML =
+        lista = lista+
             `<li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
                 <div class="d-flex flex-column">
                     <h6 class="mb-1 text-dark font-weight-bold text-sm">${dia.text}</h6>
                     <span class="text-xs">${dia.date}</span>
                 </div>
                 <div class="d-flex align-items-center ">
-                    <span class="font-weight-bold text-dark mx-2">${dia.temperature.max}</span> |  <span class="text-dark mx-2">${dia.temperature.min}</span>
+                    <span class="font-weight-bold te|xt-dark mx-2">${dia.temperature.max}</span> |  <span class="text-dark mx-2">${dia.temperature.min}</span>
                     <div class="ms-4"><i class="material-icons fs-2 me-1 rainy">${dia.icon}</i></div>
                 </div>
             </li>`
         i++
     }
 
+    week[0].innerHTML = lista
 }
 
 
